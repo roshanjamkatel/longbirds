304f1c5b6d1e773e7239ec28905152b259542e20
rjamkatel <personal@rjamkatel.com>
2020-09-02 17:04:50 -0500

    Updates

---
+++ precache-manifest.9fc2596392e0605f4e39143dd1d91009.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff0b02d10cbe1af2ec4e72de0b9f83b",
+    "url": "/longbirds/index.html"
+  },
+  {
+    "revision": "54e67c9b4f77501ad970",
+    "url": "/longbirds/static/css/main.e62ba797.chunk.css"
+  },
+  {
+    "revision": "a1ffec27674b335f504b",
+    "url": "/longbirds/static/js/2.682d1396.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/longbirds/static/js/2.682d1396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e67c9b4f77501ad970",
+    "url": "/longbirds/static/js/main.4f0ed49f.chunk.js"
+  },
+  {
+    "revision": "a9c73baf1f113becc882",
+    "url": "/longbirds/static/js/runtime-main.b4b9c834.js"
+  },
+  {
+    "revision": "69b07b34c1f97cd5d8ab7bf26fa97953",
+    "url": "/longbirds/static/media/beans-nintendo.69b07b34.jpg"
+  },
+  {
+    "revision": "8f8ca2313ed4a0433d019df02104c839",
+    "url": "/longbirds/static/media/fingers-teeth.8f8ca231.jpg"
+  },
+  {
+    "revision": "f7b4d4885756440deec2798dc9c7da4b",
+    "url": "/longbirds/static/media/longbirdsHeader.f7b4d488.jpg"
+  },
+  {
+    "revision": "6cd28662c130901fadf6f64e925446af",
+    "url": "/longbirds/static/media/tokimball.6cd28662.jpg"
+  },
+  {
+    "revision": "a3615b4a6d1e34e5923630b41b12d950",
+    "url": "/longbirds/static/media/uturn.a3615b4a.jpg"
+  }
+]);
